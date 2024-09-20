12097840c058c94b877a117eb3182c41585408ff
Deidarik <113616148+Deidarik@users.noreply.github.com>
2024-09-20 17:05:45 +0400

    package+py

--- 1_lab.py
+++ 1_lab.py
@@ -1,10 +1,9 @@
-from typing import Callable
-from gold_sec import gold_sec
-from bisec import bisect
-from tests import test_f
-from fib import fibbonachi
+from one_dimensional.gold_sec import gold_sec
+from one_dimensional.bisec import bisect
+from one_dimensional.tests import test_f
+from one_dimensional.fib import fibbonachi
 
-if __name__== "__main__":
+if __name__ == "__main__":
     print(bisect(test_f,-2.0, 2.0))
-    print(gold_sec(test_f,-2.0,2.0))
-    print(fibbonachi(test_f,-2.0,2.0))
+    print(gold_sec(test_f, -2.0, 2.0))
+    print(fibbonachi(test_f, -2.0, 2.0))
---
+++ one_dimensional/fib.py
@@ -0,0 +1,39 @@
+from typing import Callable, Tuple
+
+def fib_pair(ratio: float) -> Tuple[float,float,int]:
+    fn_1, fn = 1, 1
+    iter = 0
+    while(fn < ratio):
+        iter += 1
+        fn_1, fn = fn,fn + fn_1
+    return fn, fn_1, iter
+
+def fib_ref(fn_1: float,fn: float) -> Tuple[float,float]:
+    return fn - fn_1, fn_1
+def fibbonachi(func: Callable[[float], float], lhs: float, rhs: float, eps: float = 1.5e-6, max_iters: int = 100) -> float:
+    if(lhs > rhs): lhs, rhs = rhs, lhs
+    ratio = (rhs - lhs) / eps
+    fn_plus_1, fn, count_iter = fib_pair(ratio)
+    xl = lhs + (rhs - lhs) * (fn_plus_1 - fn) / fn_plus_1
+    xr= lhs + (rhs - lhs) * fn / fn_plus_1
+    fl = func(xl)
+    fr = func(xr)
+    fn, fn_plus_1 = fib_ref(fn, fn_plus_1)
+    count_iterw = count_iter + 1
+    while(count_iterw := count_iterw - 1):
+        if(fl > fr):
+            lhs = xl
+            xl = xr
+            fl = fr
+            xr = lhs + (rhs - lhs) * fn / fn_plus_1
+            fr = func(xr)
+        else:
+            rhs = xr
+            xr = xl
+            fr = fl
+            xl = lhs + (rhs - lhs) * (fn_plus_1 - fn) / fn_plus_1
+            fl = func(xl)
+        fn, fn_plus_1 = fib_ref(fn, fn_plus_1)
+    print(f"function probes golden ratio : {3 + count_iter}")
+    print(f"arg range: {rhs - lhs}")
+    return (lhs + rhs) * 0.5
