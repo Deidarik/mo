da8ab91b6682fce849f586ff45b9de929323efb2
Deidarik <113616148+Deidarik@users.noreply.github.com>
2024-09-20 17:44:45 +0400

    fedfed

--- one_dimensional/fib.py
+++ one_dimensional/fib.py
@@ -14,7 +14,7 @@ def fib_ref(fn_1: float,fn: float) -> Tuple[float,float]:
     return fn - fn_1, fn_1
 
 
-def fibbonachi(func: Callable[[float], float], lhs: float, rhs: float, eps: float = 1.3e-6, max_iters: int = 100) -> float:
+def fibbonachi(func: Callable[[float], float], lhs: float, rhs: float, eps: float = 1.5e-6, max_iters: int = 100) -> float:
     if(lhs > rhs): lhs, rhs = rhs, lhs
     ratio = (rhs - lhs) / eps
     fn_plus_1, fn, count_iter = fib_pair(ratio)
