1192ac18d7476e6c60f1b79605845246a43f337e
Deidarik <113616148+Deidarik@users.noreply.github.com>
2024-09-20 17:32:59 +0400

    view

--- one_dimensional/fib.py
+++ one_dimensional/fib.py
@@ -1,5 +1,6 @@
 from typing import Callable, Tuple
 
+
 def fib_pair(ratio: float) -> Tuple[float,float,int]:
     fn_1, fn = 1, 1
     count_iter = 0
@@ -8,8 +9,11 @@ def fib_pair(ratio: float) -> Tuple[float,float,int]:
         fn_1, fn = fn,fn + fn_1
     return fn, fn_1, count_iter
 
+
 def fib_ref(fn_1: float,fn: float) -> Tuple[float,float]:
     return fn - fn_1, fn_1
+
+
 def fibbonachi(func: Callable[[float], float], lhs: float, rhs: float, eps: float = 1.5e-6, max_iters: int = 100) -> float:
     if(lhs > rhs): lhs, rhs = rhs, lhs
     ratio = (rhs - lhs) / eps
