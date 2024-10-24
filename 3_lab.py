476212671b419c5bdc579058e683910dbe65cf55
Deidarik <113616148+Deidarik@users.noreply.github.com>
2024-10-24 18:48:05 +0400

    wepepepep

--- 3_lab.py
+++ 3_lab.py
@@ -1,20 +1,8 @@
-from multi_lab3 import gold_sec, bisect,test_f, bounded_f, fibbonachi, per_coord_descend, newton_raphson, conj_gradient_descend, gradient_descend
+from multi_lab3 import gold_sec, bisect,test_f, bounded_f, fibbonachi, per_coord_descend, newton_raphson, conj_gradient_descend
+from multi_lab3 import  gradient_descend, fine_first, fine_sec, PenaltyFunction
 import numpy as np
 import matplotlib.pyplot as plt
 from typing import Tuple
-
-"""
-def draw_graph(func, x0, x1, nsteps=100, points:Tuple[Tuple[float,float], ...] = None):
-    xs = np.linspace(x0, x1, nsteps)
-    ys = np.array(tuple(func(x)for x in xs.flat))
-    plt.plot(xs, ys)
-    if points:
-        for x,y in points:
-            plt.plot(x, y, 'o')
-    plt.xlabel('x')
-    plt.xlabel('y')
-    plt.show()
-"""
 
 if __name__ == "__main__":
     init_x_ar = np.array([0, 0], dtype = float)
@@ -27,6 +15,9 @@ if __name__ == "__main__":
     print(per_coord_descend(test_f, x_start))
     print(gradient_descend(test_f, x_start))
     print(conj_gradient_descend(test_f, x_start))
-    print(newton_raphson(bounded_f, x_nr_start))
-    #meme = fibbonachi(test_f, next_x_ar, init_x_ar)
-    #print(draw_graph(test_f, next_x_ar, init_x_ar, 100, [(meme, test_f(meme))]))
+    target = PenaltyFunction()
+    target.target = test_f
+    target.add_bound(fine_first)
+    target.add_bound(fine_sec)
+    print(newton_raphson(target.__call__, x_nr_start))
+    target.clear_boundaries
--- multi_lab3/__init__.py
+++ multi_lab3/__init__.py
@@ -5,4 +5,5 @@ from .coord_descendence import per_coord_descend
 from .grad import gradient_descend
 from .conj_grad import conj_gradient_descend
 from .newton import newton_raphson
-from .tests import bounded_f, test_f
+from .tests import bounded_f, test_f, fine_first, fine_sec
+from .penalty import PenaltyFunction
